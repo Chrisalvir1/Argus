6f605afc9a975808a5218aee94b66f6b2101c300
Christopher Alvir <chrisalvir@Mac-mini-de-MC-3.local>
2026-07-16 11:08:38 -0600

    fix(test): update version in test suite for v1.4.2

--- custom_components/argus/const.py
+++ custom_components/argus/const.py
@@ -1,9 +1,9 @@
-"""Constants for the Argus Home Hub v1.4.1"""
+"""Constants for the Argus Home Hub v1.4.2"""
 from homeassistant.const import CONF_NAME, CONF_CODE
 
 DOMAIN = "argus"
 NAME = "Argus Home Hub"
-VERSION = "1.4.1"
+VERSION = "1.4.2"
 
 PLATFORMS = ["alarm_control_panel", "switch"]
 DATA_PANELS = "panels"
